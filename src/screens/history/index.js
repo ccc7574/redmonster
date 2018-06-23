fe07368c07e0cfb2e88ad45fb06e904bad5928d7
mashaodong <1030724610@qq.com>
2018-06-23 15:00:30 +0800

    update

--- src/screens/history/index.js
+++ src/screens/history/index.js
@@ -15,11 +15,24 @@ import {
   Icon,
   Card,
   CardItem,
-  ListItem
+  ListItem,
+  View
 } from "native-base";
 import { VictoryLine,VictoryChart,VictoryTheme} from "victory-native";
+import { Image } from "react-native";
 import styles from "./styles";
 const Item = Picker.Item;
+
+const MoneyCell = ({ moneyNum, moneyUnit }) => {
+  return (
+    <View style={styles.moneyItem}>
+      <View style={styles.moneyItemBox}>
+        <Text style={styles.moneyItemNum}>{moneyNum}</Text>
+      </View>
+      <Text style={styles.moneyItemUnit}>{moneyUnit}</Text>
+    </View>
+  );
+};
 
 class History extends Component {
   constructor(props) {
@@ -29,15 +42,71 @@ class History extends Component {
       tab2: false,
       tab3: true,
       tab4: false,
-      selected2: undefined,
+      selected: undefined,
+      timeSection: "hour",
     };
   }
 
   onValueChange2=(value)=>{
     this.setState({
-      selected2: value
+      selected: value
     });
-  }
+  };
+
+  renderMoney = () => {
+    return (
+      <View style={styles.moneyContainer}>
+        <MoneyCell moneyNum={0} moneyUnit={"千"}/>
+        <MoneyCell moneyNum={9} moneyUnit={"百"}/>
+        <MoneyCell moneyNum={2} moneyUnit={"十"}/>
+        <MoneyCell moneyNum={3} moneyUnit={"元"}/>
+        <Text style={styles.point}>.</Text>
+        <MoneyCell moneyNum={2} moneyUnit={"角"}/>
+        <MoneyCell moneyNum={5} moneyUnit={"分"}/>
+      </View>
+    );
+  };
+
+  renderChart = () => {
+    const { timeSection } = this.state;
+    return (
+      <View style={styles.chartBox}>
+        <Text style={styles.chartTitle}>走势图</Text>
+        <View style={styles.timeSection}>
+          <Button
+            style={timeSection === "hour" ? styles.timeLineActive : styles.timeLine}
+            onPress={() => this.changeTimeSection("hour")}
+          >
+            <Text style={timeSection === "hour" ? styles.timeLineTxtActive : styles.timeLineTxt}>时</Text>
+          </Button>
+          <Button
+            style={timeSection === "day" ? styles.timeLineActive : styles.timeLine}
+            onPress={() => this.changeTimeSection("day")}
+          >
+            <Text style={timeSection === "day" ? styles.timeLineTxtActive : styles.timeLineTxt}>天</Text>
+          </Button>
+          <Button
+            style={timeSection === "week" ? styles.timeLineActive : styles.timeLine}
+            onPress={() => this.changeTimeSection("week")}
+          >
+            <Text style={timeSection === "week" ? styles.timeLineTxtActive : styles.timeLineTxt}>周</Text>
+          </Button>
+          <Button
+            style={timeSection === "month" ? styles.timeLineActive : styles.timeLine}
+            onPress={() => this.changeTimeSection("month")}
+          >
+            <Text style={timeSection === "month" ? styles.timeLineTxtActive : styles.timeLineTxt}>月</Text>
+          </Button>
+        </View>
+      </View>
+    );
+  };
+
+  changeTimeSection = (type) => {
+    this.setState({
+      timeSection: type
+    });
+  };
 
   render() {
     return (
@@ -59,43 +128,55 @@ class History extends Component {
           <Right/>
         </Header>
 
-        <Content padder>
-          <Card style={styles.mb}>
-            <CardItem>
-              <Left>
-                <Text style={{ color: "#b09872" }}>精品普洱茶</Text>
-              </Left>
-              <Right>
-                <Form>
-                  <Picker
-                    mode="dropdown"
-                    iosIcon={<Icon name="ios-arrow-down-outline"/>}
-                    style={{ width: undefined }}
-                    placeholder="选择其他产品"
-                    textStyle={{ color: "#b09872" }}
-                    itemStyle={{
-                      backgroundColor: "#d3d3d3",
-                      marginLeft: 0,
-                      paddingLeft: 10
-                    }}
-                    itemTextStyle={{ color: "#788ad2" }}
-                    selectedValue={this.state.selected2}
-                    onValueChange={this.onValueChange2}
-                  >
-                    <Item label="精品红酒" value="key0"/>
-                    <Item label="精品铁观音" value="key1"/>
-                    <Item label="精品碧螺春" value="key2"/>
-                  </Picker>
-                </Form>
-              </Right>
-            </CardItem>
-          </Card>
-          <ListItem>
-            <Left>
-              <Text>最新价格: </Text>
-            </Left>
-            <Right><Text style={styles.priceTag}>481.98元</Text></Right>
-          </ListItem>
+        <Content>
+          <View style={styles.buyBox}>
+            <Text style={styles.fontWhite}>当前价：¥402.83</Text>
+            <Button block bordered style={styles.buyButton} onPress={() => this.props.navigation.navigate("Team")}>
+              <Text style={styles.buyButtonTxt}>立刻购买</Text>
+            </Button>
+          </View>
+
+          { this.renderMoney() }
+
+          { this.renderChart() }
+
+
+          {/*<Card style={styles.mb}>*/}
+            {/*<CardItem>*/}
+              {/*<Left>*/}
+                {/*<Text style={{ color: "#b09872" }}>精品普洱茶</Text>*/}
+              {/*</Left>*/}
+              {/*<Right>*/}
+                {/*<Form>*/}
+                  {/*<Picker*/}
+                    {/*mode="dropdown"*/}
+                    {/*iosIcon={<Icon name="ios-arrow-down-outline"/>}*/}
+                    {/*style={{ width: undefined }}*/}
+                    {/*placeholder="选择其他产品"*/}
+                    {/*textStyle={{ color: "#b09872" }}*/}
+                    {/*itemStyle={{*/}
+                      {/*backgroundColor: "#d3d3d3",*/}
+                      {/*marginLeft: 0,*/}
+                      {/*paddingLeft: 10*/}
+                    {/*}}*/}
+                    {/*itemTextStyle={{ color: "#788ad2" }}*/}
+                    {/*selectedValue={this.state.selected}*/}
+                    {/*onValueChange={this.onValueChange2}*/}
+                  {/*>*/}
+                    {/*<Item label="精品红酒" value="key0"/>*/}
+                    {/*<Item label="精品铁观音" value="key1"/>*/}
+                    {/*<Item label="精品碧螺春" value="key2"/>*/}
+                  {/*</Picker>*/}
+                {/*</Form>*/}
+              {/*</Right>*/}
+            {/*</CardItem>*/}
+          {/*</Card>*/}
+          {/*<ListItem>*/}
+            {/*<Left>*/}
+              {/*<Text>最新价格: </Text>*/}
+            {/*</Left>*/}
+            {/*<Right><Text style={styles.priceTag}>481.98元</Text></Right>*/}
+          {/*</ListItem>*/}
           <VictoryChart
             theme={VictoryTheme.material}
           >
@@ -130,37 +211,44 @@ class History extends Component {
             </CardItem>
           </Card>
         </Content>
-        <Footer>
-          <FooterTab>
-            <Button
-              active={this.state.tab1}
-              onPress={() => this.props.navigation.navigate("Home")}
-              vertical
-            >
-              <Icon active={this.state.tab1} name="apps"/>
-              <Text>首页</Text>
-            </Button>
-            <Button active={this.state.tab2}
+
+        <Footer style={styles.footerTab}>
+          <FooterTab style={styles.footerTab}>
+            <Button style={styles.footerButton}
+                    onPress={() => this.props.navigation.navigate("Settings")}
                     vertical
+            >
+              <View>
+                <Image style={styles.footerImageHL} source={require("../../../assets/home/hl-active.png")}/>
+                <Text style={styles.footerText}>红炉</Text>
+              </View>
+            </Button>
+            <Button vertical style={styles.footerButton}
                     onPress={() => this.props.navigation.navigate("WareHouse")}>
-              <Icon name="camera" active={this.state.tab2}/>
-              <Text>仓库</Text>
+              <View>
+                <Image style={styles.footerImage} source={require("../../../assets/home/storage.png")}/>
+                <Text style={styles.footerText}>仓库</Text>
+              </View>
             </Button>
-            <Button
-              active={this.state.tab3}
-              onPress={() => this.props.navigation.navigate("History")}
-              vertical
+            <Button style={styles.footerButton}
+                    active={this.state.tab3}
+                    onPress={() => this.props.navigation.navigate("History")}
+                    vertical
             >
-              <Icon active={this.state.tab3} name="man"/>
-              <Text>团队</Text>
+              <View>
+                <Image style={styles.footerImage} source={require("../../../assets/home/team.png")}/>
+                <Text style={styles.footerText}>团队</Text>
+              </View>
             </Button>
-            <Button
-              active={this.state.tab4}
-              onPress={() => this.props.navigation.navigate("Login")}
-              vertical
+            <Button style={styles.footerButton}
+                    active={this.state.tab4}
+                    onPress={() => this.props.navigation.navigate("Coupon")}
+                    vertical
             >
-              <Icon active={this.state.tab4} name="contact" />
-              <Text>我的</Text>
+              <View>
+                <Image style={styles.footerImage} source={require("../../../assets/home/mine.png")}/>
+                <Text style={styles.footerText}>我的</Text>
+              </View>
             </Button>
           </FooterTab>
         </Footer>
--- src/screens/history/styles.js
+++ src/screens/history/styles.js
@@ -1,10 +1,12 @@
 const React = require("react-native");
 const { Dimensions, Platform } = React;
 const deviceHeight = Dimensions.get("window").height;
+const goldColor = "#DABB84";
 
 export default {
   container: {
-    backgroundColor: "#FFF"
+    // backgroundColor: "#FFF",
+    backgroundColor: "#000",
   },
   mb: {
     marginBottom: 15
@@ -40,7 +42,7 @@ export default {
     marginRight: 15
   },
   priceTag:{
-    color:"#b09872"
+    color: goldColor
   },
   chartContainer:{
     flex: 1,
@@ -51,5 +53,130 @@ export default {
   chart:{
     width: 200,
     height: 200,
-  }
+  },
+  footerButton: {
+    width: 93, height: 49
+  },
+  footerImageHL: {
+    width: 25, height: 17
+  },
+  footerImage: {
+    width: 20, height: 20
+  },
+  footerText: {
+    fontSize: 10, paddingTop: 6, color: "#7f7f7f"
+  },
+  footerTab: {
+    backgroundColor: "#1a1a1a",
+    borderTopWidth: 0
+  },
+  buyBox: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#666"
+  },
+  fontGold: {
+    color: goldColor,
+  },
+  fontWhite: {
+    color: "#ccc"
+  },
+  fontGrey: {
+    color: "#7F7F7F"
+  },
+  buyButton: {
+    width: 96,
+    height: 38,
+    borderWidth: 1,
+    borderColor: goldColor,
+    borderRadius: 18
+  },
+  buyButtonTxt: {
+    color: goldColor,
+    fontSize: 15,
+  },
+  moneyContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 30,
+  },
+  moneyItem: {
+    width: 40,
+    marginLeft: 5
+  },
+  moneyItemBox: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: 40,
+    height: 40,
+    backgroundColor: "#1a1a1a",
+    borderRadius: 10
+  },
+  moneyItemNum: {
+    color: goldColor,
+    fontSize: 26,
+    textAlign: "center"
+  },
+  moneyItemUnit: {
+    marginTop: 5,
+    color: "#6d6d6d",
+    fontSize: 14,
+    textAlign: "center"
+  },
+  point: {
+    color: goldColor,
+    marginLeft: 5,
+    paddingBottom: 14,
+    fontSize: 28,
+    fontWeight: "bold"
+  },
+  chartBox: {
+    padding: 10,
+  },
+  chartTitle: {
+    color: "#666",
+  },
+  timeSection: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  timeLineActive: {
+    height: 28,
+    width: 50,
+    padding: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    marginLeft: 8,
+    borderRadius: 14,
+    borderColor: goldColor,
+    borderWidth: 1,
+    backgroundColor: "transparent",
+  },
+  timeLine: {
+    height: 28,
+    width: 50,
+    padding: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    marginLeft: 8,
+    borderRadius: 14,
+    borderWidth: 0,
+    backgroundColor: "transparent",
+  },
+  timeLineTxt: {
+    color: "#666",
+    fontSize: 16,
+  },
+  timeLineTxtActive: {
+    color: goldColor,
+    fontSize: 16,
+  },
 };
