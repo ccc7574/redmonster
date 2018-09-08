8b347e17452fada2660933ef74b470fb1d4930c7
Louis Weng <louis396@hotmail.com>
2018-09-08 15:07:02 +0800

    fix nick name

--- src/screens/utils.js
+++ src/screens/utils.js
@@ -1,4 +1,36 @@
 'use strict'
+let domain = 'http://win';
 
-const serverAPI = 'http://windapi-server-dev.chinanorth.cloudapp.chinacloudapi.cn:8000';
-module.exports = {serverAPI}
+function getRelation(str1, str2) {
+  if (str1 === str2) {
+    console.warn('Two path are equal!');  // eslint-disable-line
+  }
+  const arr1 = str1.split('/');
+  const arr2 = str2.split('/');
+  if (arr2.every((item, index) => item === arr1[index])) {
+    return 1;
+  } else if (arr1.every((item, index) => item === arr2[index])) {
+    return 2;
+  }
+  return 3;
+}
+
+function getRenderArr(routes) {
+  let renderArr = [];
+  renderArr.push(routes[0]);
+  for (let i = 1; i < routes.length; i += 1) {
+    let isAdd = false;
+    // 是否包含
+    isAdd = renderArr.every(item => getRelation(item, routes[i]) === 3);
+    // 去重
+    renderArr = renderArr.filter(item => getRelation(item, routes[i]) !== 1);
+    if (isAdd) {
+      renderArr.push(routes[i]);
+    }
+  }
+  return renderArr;
+}
+const {sName} = require('./sutils');
+
+const serverAPI = `${domain}dapi-server${sName}:8000`;
+module.exports = {domain,serverAPI}
