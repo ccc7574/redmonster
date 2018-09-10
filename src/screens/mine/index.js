0021a52f95a43ac9d9dc32f97f53ba3fa2bae8bf
mashaodong <1030724610@qq.com>
2018-09-10 23:29:51 +0800

    update

--- src/screens/mine/index.js
+++ src/screens/mine/index.js
@@ -56,9 +56,13 @@ class Mine extends Component {
   };
 
   handleConfirm = () => {
-    AsyncStorage.clear();
-    Alert.alert("退出成功!");
-    this.props.navigation.navigate("Home");
+    this.setState({
+      modalVisible: false
+    }, () => {
+      AsyncStorage.clear();
+      Alert.alert("退出成功!");
+      this.props.navigation.navigate("Home");
+    });
   };
 
   closeModal = () => {
